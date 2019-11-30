c5329f839b1002fa37029d5179496d50ae3d394b
Iván Martínez <ivanmartinez33@outlook.com>
2019-11-30 17:53:31 -0600

    Se agregó el playground para las variables y constantes

---
+++ variables_y_constantes.playground/Contents.swift
@@ -0,0 +1,21 @@
+/*
+ Variables y constantes
+ 
+ Tipos de datos:
+ Int
+ Float
+ Double
+ String
+*/
+
+// Variables - Para datos que van a cambiar
+var nombre: String = "Iván" // Anotación de tipo
+var edad = 19 // Inferencia de tipo
+
+nombre = "Edgar" // Se pueden re-asignar los valores de las variables
+
+// Constantes - Para datos que no van a cambiar
+let pi = 3.14159265358979323846264338327950288419716
+
+// Descomenta la siguiente línea para ver el error
+// pi = 3
