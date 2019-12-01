7a451f9adfa65b2836020b9a1af8dbdaef490f85
Iván Martínez <ivanmartinez33@outlook.com>
2019-11-30 18:12:55 -0600

    Se agregó el playground de concatenación e interpolación

---
+++ concatenacion_e_interpolacion.playground/Contents.swift
@@ -0,0 +1,14 @@
+/*
+    Concatenación e interpolación
+*/
+
+// Concatenación es la suma de Strings
+let cadena1 = "Hola"
+let cadena2 = "Mundo"
+
+let cadenaFinal = cadena1 + " " + cadena2
+
+
+// Interpolación es insertar un String u otro tipo de dato dentro de un String
+let titulo = 1984
+print("\(titulo) es un buen libro")
