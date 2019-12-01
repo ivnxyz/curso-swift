fbf1904da7a51a7a5b17b912a0068f6542a55f5b
Iván Martínez <ivanmartinez33@outlook.com>
2019-11-30 18:09:47 -0600

    Se agregó el playground de ciclos

--- ciclos.playground/Contents.swift
+++ ciclos.playground/Contents.swift
@@ -1,5 +1,23 @@
 /*
- Ciclos for, while y repeat while
+ Ciclos for y ciclos while
 */
 
 // Recorrer una lista
+let nombres = ["John", "Paul", "George", "Ringo"]
+
+for nombre in nombres { // nombre es la variable de control
+    print("\(nombre) es un Beatle")
+}
+
+// Recorrer un rango
+for n in 1...5 {
+    print(n)
+}
+
+// Ciclos while
+var contador = 0
+
+while contador < 10 {
+    contador += 1
+    print(contador)
+}
