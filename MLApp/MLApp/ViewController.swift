2754f10fc5ed2b08688511f044bf8f81ff898df8
Iván Martínez <ivanmartinez33@outlook.com>
2019-12-03 19:28:55 -0600

    Ya se pueden tomar fotos y se cambió la versión de iOS target

--- MLApp/MLApp/ViewController.swift
+++ MLApp/MLApp/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Atributos
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    private var imagePickerController: UIImagePickerController?
+    
+    // MARK: - ViewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +30,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func selectAnImageButtonPressed(_ sender: UIButton) {
+        // Crear un controlador para la cámara y configurarlo
+        imagePickerController = UIImagePickerController()
+        imagePickerController?.delegate = self
+        imagePickerController?.sourceType = .camera
+        
+        // Mostrar el controlador
+        present(imagePickerController!, animated: true, completion: nil)
     }
     
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    // MARK: - UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // Ocultar el controlador
+        picker.dismiss(animated: true, completion: nil)
+        
+        // Mostrar la imagen editada
+        imageView.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+    }
+    
+    
+}
+
+extension ViewController: UINavigationControllerDelegate {
+    
+    // MARK: - UINavigationControllerDelegate
+    
 }
 
