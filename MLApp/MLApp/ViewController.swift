ba631ab50d50a31a58ffb4bbb3f38d7e1f6a4c3e
Iván Martínez <ivanmartinez33@outlook.com>
2019-12-03 18:47:43 -0600

    Proyecto creado

---
+++ MLApp/MLApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLApp
+//
+//  Created by Iván Martínez on 03/12/19.
+//  Copyright © 2019 Iván Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
